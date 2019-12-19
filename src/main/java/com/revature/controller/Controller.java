0e23c6a4bfc57189ce0bd61efebc83892a7ddde2
Dillon Phillips <edmond.dillon.phillips@gmail.com>
2019-12-18 19:40:00 -0500

    Added pseudo code for routes in controller

--- src/main/java/com/revature/controller/Controller.java
+++ src/main/java/com/revature/controller/Controller.java
@@ -1,5 +1,38 @@
 package com.revature.controller;
 
 public class Controller {
-
+	
+	Boolean loggedIn; //login status
+	String username; //username of user
+	Integer password; //password of user
+	String checkBalance; // checks to see if user wants to check balance
+	
+	
+	
+	//default constructor
+	Controller(){
+		super();
+	}
+	
+	//constructor (overloading)
+	Controller(Boolean loggedIn, String username, Integer password){
+		this.loggedIn = loggedIn;
+		this.username = username;
+		this.password = password;
+	}
+	
+	//if the user hits the login/logout route. Perform some logic
+	
+	//if the user hits the view balance route. Perform some logic.
+	
+	// IF a user hits the withdraw money route. Perform some logic.
+	
+	// If a user hits the deposit money route. Perform some logic.
+	
+	// If a user hits the view all transactions route. Perform some logic.
+	
+	// If a user hits the transfer money route. Perform some logic.
+	
+	
+	
 }
