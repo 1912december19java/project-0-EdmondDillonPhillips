44882d42bccce7b9b0aff78b122c31ce0c8974a4
Dillon Phillips <edmond.dillon.phillips@gmail.com>
2019-12-18 20:02:36 -0500

    Added pseudo code to services. Going to be adding in simple print methods until given further information

---
+++ src/main/java/com/revature/service/Service.java
@@ -0,0 +1,19 @@
+package com.revature.service;
+
+public class Service {
+	//simple print statement for login logout
+	
+	//simple print statement for view balance
+	
+	//simple print statement for withdraw money
+	
+	//simple print statement for deposit money
+	
+	//simple print statement for registering
+	
+	//simple print statement to view transaction history
+	
+	//simple print statement to transfer money to another account
+	
+	
+}
